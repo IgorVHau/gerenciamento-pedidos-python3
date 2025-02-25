7e076cc2f4640ec93291bdcef883266fdc434c61
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-02-25 18:47:41 -0300

    feat/Implementação do método Observer no main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from cliente import Cliente
 from item import Item
-from pedido.pedido_retirada import PedidoRetirada
 from pedido.pedido_delivery import PedidoDelivery
-from pagamento.pagamento_cartao import PagamentoCartao
-from pagamento.pagamento_pix import PagamentoPIX
 from pagamento.pagamento_factory import PagamentoFactory
-from notificacao.notificacao_email import NotificacaoEmail
-from notificacao.notificacao_sms import NotificacaoSMS
 from notificacao.notificacao_facade import NotificacaoFacade
+from observador.observador_status import ObservadorStatus
 
 
 cliente = Cliente("Mario", "Alura")
@@ -16,20 +12,21 @@ item_dois = Item("Refrigerante", 5.00)
 itens = [item_um, item_dois]
 
 taxa_entrega = 7.50
-pedido_retirada = PedidoRetirada(cliente, itens)
-pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
+pedido = PedidoDelivery(cliente, itens, taxa_entrega)
 
-valor_pedido = pedido_retirada.calcular_total()
-# pagamento_cartao = PagamentoCartao().processar(valor_pedido)
-# pagamento_pix = PagamentoPIX().processar(valor_pedido)
+valor_pedido = pedido.calcular_total()
+
 tipo_pagamento = input("Escolha o método de pagamento: ").lower()
-pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento)
-pagamento.processar(valor_pedido)
+pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento).processar(valor_pedido)
 
-MENSAGEM = "Seu pedido saiu para a entrega!"
-# notificacao_email = NotificacaoEmail().enviar_notificacao(cliente, MENSAGEM)
-# notificacao_sms = NotificacaoSMS().enviar_notificacao(cliente, MENSAGEM)
-notificacoes = NotificacaoFacade().enviar_notificacoes(cliente, MENSAGEM)
+MENSAGEM_PAGO = "O pagamento foi confirmado."
+MENSAGEM_PREPARANDO = "O pedido está sendo preparado."
+MENSAGEM_ENVIADO = "O pedido saiu para a entrega."
 
-pedido_delivery.status = "Pedido confirmado!"
-notificacoes = NotificacaoFacade().enviar_notificacoes(cliente, pedido_delivery.status)
+notificacoes = NotificacaoFacade()
+observador = ObservadorStatus(notificacoes)
+pedido.adicionar_observadores(observador)
+
+pedido.status = MENSAGEM_PAGO
+pedido.status = MENSAGEM_PREPARANDO
+pedido.status = MENSAGEM_ENVIADO
