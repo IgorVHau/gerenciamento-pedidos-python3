9c7bb1051a1705a604aa996e81e9f3cc20a94a65
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-02-25 17:07:00 -0300

    feat/Atualização de status do pedido pelo princípio da inversão de dependência - D

--- pagamento/pagamento_cartao.py
+++ pagamento/pagamento_cartao.py
@@ -3,4 +3,4 @@ from pagamento.pagamento import Pagamento
 
 class PagamentoCartao(Pagamento):
     def processar(self, valor):
-        print(f"Processando pagamento de R$ {valor:.2f} via cartão.")
+        print(f"Processando pagamento de R$ {valor:.2f} via cartão.\n")
